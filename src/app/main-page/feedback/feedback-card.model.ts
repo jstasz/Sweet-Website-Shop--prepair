a4515fd9f5e28c7ed04ab589afc7690284f11570
jstasz <jszajuk@gmail.com>
2022-11-04 11:11:02 +0100

    feedback component

---
+++ src/app/main-page/feedback/feedback-card.model.ts
@@ -0,0 +1,8 @@
+export class FeedbackCard {
+
+    constructor(public userName: string, public description: string) {
+
+        this.userName = userName;
+        this.description = description;
+    }
+}
