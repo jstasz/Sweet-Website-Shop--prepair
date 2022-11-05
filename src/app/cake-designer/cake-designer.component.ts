f3134d28b3df53a34ba5bbbef96e8ee2b18ef6e7
jstasz <jszajuk@gmail.com>
2022-11-05 23:12:38 +0100

    implement cake designer instruction

--- src/app/cake-designer/cake-designer.component.ts
+++ src/app/cake-designer/cake-designer.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+class CreateCakeElement {
+  constructor(public elementName: string, public elementIcon: string) {
+    this.elementName = elementName,
+      this.elementIcon = elementIcon
+  }
+}
+
 @Component({
   selector: 'app-cake-designer',
   templateUrl: './cake-designer.component.html',
@@ -7,6 +14,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CakeDesignerComponent implements OnInit {
 
+  createCakeInstruction: CreateCakeElement[] = [
+    new CreateCakeElement('type', 'fa-solid fa-cake-candles'),
+    new CreateCakeElement('size', 'fa-solid fa-scale-unbalanced'),
+    new CreateCakeElement('flavour', 'fa-solid fa-cookie-bite'),
+    new CreateCakeElement('date', 'fa-solid fa-calendar-days')
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
