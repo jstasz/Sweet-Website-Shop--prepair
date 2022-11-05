f2eef0733c16d74c15c875d10187e3e63cdc46ac
jstasz <jszajuk@gmail.com>
2022-11-05 19:11:26 +0100

    branch conflicts

--- src/app/cake-designer/cake-designer.component.ts
+++ src/app/cake-designer/cake-designer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cake-desgner',
+  selector: 'app-cake-designer',
   templateUrl: './cake-designer.component.html',
   styleUrls: ['./cake-designer.component.scss']
 })
