e836089536749d3e5d1ddbb514adcf3c6b88e5a4
jstasz <jszajuk@gmail.com>
2022-11-07 20:29:07 +0100

    working with cake flavour - template and scss

--- src/app/cake-designer/flavour/flavour.component.ts
+++ src/app/cake-designer/flavour/flavour.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+class CakeFlavour {
+  constructor(public name: string, public image: string) {
+    this.name = name;
+    this.image = image;
+  }
+}
+
 @Component({
   selector: 'app-flavour',
   templateUrl: './flavour.component.html',
@@ -7,9 +14,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlavourComponent implements OnInit {
 
+  cakeFlavours: CakeFlavour[] = [
+    new CakeFlavour('vanilla', '../../../assets/img/create-cake/flavour/vanilla.webp'),
+    new CakeFlavour('chocolate', '../../../assets/img/create-cake/flavour/chocolate.webp'),
+    new CakeFlavour('rainbow', '../../../assets/img/create-cake/flavour/rainbow.jpeg')
+  ]
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
