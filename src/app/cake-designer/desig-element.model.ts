2fe64f130d4fc5e04c6a8048437a2f751745eb94
jstasz <jszajuk@gmail.com>
2022-11-08 20:43:29 +0100

    create design-element model to render cake elements to choose

---
+++ src/app/cake-designer/desig-element.model.ts
@@ -0,0 +1,7 @@
+export class DesignElement {
+    constructor(public name: string, public imagePath: string) {
+        this.name = name;
+        this.imagePath = imagePath;
+    }
+}
+
--- src/app/cake-designer/flavour/flavour.component.ts
+++ src/app/cake-designer/flavour/flavour.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-class CakeFlavour {
-  constructor(public name: string, public image: string) {
-    this.name = name;
-    this.image = image;
-  }
-}
+import { DesignElement } from '../desig-element.model';
 
 @Component({
   selector: 'app-flavour',
@@ -14,10 +8,10 @@ class CakeFlavour {
 })
 export class FlavourComponent implements OnInit {
 
-  cakeFlavours: CakeFlavour[] = [
-    new CakeFlavour('vanilla', '../../../assets/img/create-cake/flavour/vanilla.webp'),
-    new CakeFlavour('chocolate', '../../../assets/img/create-cake/flavour/chocolate.webp'),
-    new CakeFlavour('rainbow', '../../../assets/img/create-cake/flavour/rainbow.jpeg')
+  cakeFlavours: DesignElement[] = [
+    new DesignElement('vanilla', '../../../assets/img/create-cake/flavour/vanilla.webp'),
+    new DesignElement('chocolate', '../../../assets/img/create-cake/flavour/chocolate.webp'),
+    new DesignElement('rainbow', '../../../assets/img/create-cake/flavour/rainbow.jpeg')
   ]
 
   constructor() { }
