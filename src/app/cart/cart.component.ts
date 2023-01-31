1ab21a2a46888df0d8d1dd4a477922dbd2d9927e
jstasz <jszajuk@gmail.com>
2023-01-31 14:36:41 +0100

    creating initial template and  styles for cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from './cart.model';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  cart: Cart = { items: [{
+    product: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm2GEwY-QGtOjmIVZSeQEfo-7QJGQpVRtZO4KO70NizPiE5TTj4D3HMZgJiiG4Fqd32n0&usqp=CAU',
+    name: 'cake',
+    price: 150,
+    quantity: 2,
+    id: 1
+  },
+  {
+    product: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm2GEwY-QGtOjmIVZSeQEfo-7QJGQpVRtZO4KO70NizPiE5TTj4D3HMZgJiiG4Fqd32n0&usqp=CAU',
+    name: 'cake',
+    price: 100,
+    quantity: 5,
+    id: 1
+  }]}
+  dataSource: CartItem[] = []
+  totalPrice: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = this.cart.items
   }
 
+  getTotalPrice(item: CartItem) {
+    return item.quantity * item.price
+  }
+
+  getTotalCart(items: CartItem[]) {
+    return items.map((item) => item.price * item.quantity).reduce((prev, cur) => prev + cur, 0)
+  }
 }
