3ee1608025556d3911c3b7e9abdc6d55004acf01
jstasz <jszajuk@gmail.com>
2023-02-03 11:28:21 +0100

    moving shop products to shop service and add new products

--- src/app/shop-online/shop-products/shop-product/shop-product.component.ts
+++ src/app/shop-online/shop-products/shop-product/shop-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Cart,} from 'src/app/cart/cart.model';
 import { CartService } from 'src/app/cart/cart.service';
+import { ShopOnlineService } from '../../shop-online.servis';
 import { Layout } from '../products.model';
 import { ShopProduct } from './product.model';
 
@@ -12,25 +12,15 @@ import { ShopProduct } from './product.model';
 export class ShopProductComponent implements OnInit {
 
 @Input() layout: Layout = 'grid'
+shopProducts : ShopProduct[] = [];
 
-shopProducts : ShopProduct[] = [
-{id: 1, name: 'Cake1', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque', imagePath: 'https://cakebycourtney.com/wp-content/uploads/2015/08/Cookies-and-Cream-Cake-4-e1563420600943.jpg', price: 50, category: 'cakes', quantity: 1},
-{id: 2, name: 'Cake2', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque', imagePath: 'https://cakebycourtney.com/wp-content/uploads/2015/08/Cookies-and-Cream-Cake-4-e1563420600943.jpg', price: 50, category: 'cakes', quantity: 1},
-{id: 3, name: 'Cake3', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque', imagePath: 'https://cakebycourtney.com/wp-content/uploads/2015/08/Cookies-and-Cream-Cake-4-e1563420600943.jpg', price: 50, category: 'cakes', quantity: 1},
-{id: 4, name: 'Cake4', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque', imagePath: 'https://cakebycourtney.com/wp-content/uploads/2015/08/Cookies-and-Cream-Cake-4-e1563420600943.jpg', price: 50, category: 'cakes', quantity: 1},
-{id: 5, name: 'Cake5', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque', imagePath: 'https://cakebycourtney.com/wp-content/uploads/2015/08/Cookies-and-Cream-Cake-4-e1563420600943.jpg', price: 50, category: 'cakes', quantity: 1},
-{id: 6, name: 'Cake6', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque', imagePath: 'https://cakebycourtney.com/wp-content/uploads/2015/08/Cookies-and-Cream-Cake-4-e1563420600943.jpg', price: 50, category: 'cakes', quantity: 1}
-]
-
-@Output() addToCart = new EventEmitter<ShopProduct>();
-
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private shopOnlineService: ShopOnlineService) { }
 
   ngOnInit(): void {
+    this.shopProducts = this.shopOnlineService.shopProducts
   }
 
   onAddToCart(product: ShopProduct) {
     this.cartService.addToCart(product)
   }
-
 }
