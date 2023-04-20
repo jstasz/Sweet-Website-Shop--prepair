8480d7d06ef572390d5a33d18d82c463cfe8dde0
jstasz <jszajuk@gmail.com>
2023-04-20 15:46:44 +0200

    fixing to large scss file - cake designer

--- src/app/cake-designer/summary/summary.component.ts
+++ src/app/cake-designer/summary/summary.component.ts
@@ -8,6 +8,7 @@ import { Cake } from '../desig-element.model';
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit {
 
