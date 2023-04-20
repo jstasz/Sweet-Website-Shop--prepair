b284174b2bb6ad336598f02c07d8bce2322a2aee
jstasz <jszajuk@gmail.com>
2023-04-20 15:52:30 +0200

    fixing to large scss file - header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { HeaderService, Mode } from './header.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', './header-hamburger.scss']
 })
 export class HeaderComponent implements OnInit {
 
