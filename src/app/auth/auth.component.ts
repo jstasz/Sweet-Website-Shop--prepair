2a5c2c073d3a91a0b8108207d5d10b8602882450
jstasz <jszajuk@gmail.com>
2023-04-21 13:31:58 +0200

    adding firebase auth for signup and login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+
+const auth = getAuth();
 
 @Component({
   selector: 'app-auth',
@@ -9,6 +12,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AuthComponent implements OnInit {
   isLoginMode: boolean = false;
   authForm!: FormGroup;
+  isLogged: boolean = false;
+  errorMessage: string = '';
 
   constructor() { }
 
@@ -20,7 +25,54 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit() {
-    
+    const auth = getAuth();
+    let email = this.authForm.value.email;
+    let password = this.authForm.value.password;
+
+    if (!this.isLoginMode) {
+      this.signUpNewUser(email, password);
+    } else {
+      this.loginUser(email, password);
+    }
+
+    this.authForm.reset();
+  }
+
+  signUpNewUser(email: string, password: string) {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      this.isLogged = true;
+    })
+    .catch((error) => {
+      let errorMessage = 'something go wrong';
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          errorMessage = 'The email address is already in use by another account';
+          break;
+      }
+      this.errorMessage = errorMessage;
+    });
+  }
+
+  loginUser(email: string, password: string) {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      this.isLogged = true;
+    })
+    .catch((error) => { 
+      let errorMessage = 'an unknown error';
+      switch (error.code) {
+        case 'auth/user-not-found':
+          errorMessage = 'There is no user record corresponding to this email. The user may have been deleted';
+          break;
+        case 'auth/wrong-password':
+          errorMessage = 'The password is invalid or the user does not have a password';
+          break;
+      }
+      this.errorMessage = errorMessage;
+    });
   }
 
   onSwitchMode() {
